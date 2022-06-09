82e3c958d9957dfaf65403cff6ed66d0ccf77db0
Afsaruddin12133 <afsaruddin12133@gmail.com>
2022-06-09 17:19:42 +0600

    Update api

--- index.js
+++ index.js
@@ -15,7 +15,22 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.PRODUCT_USER}:${process.env.PRODUCT_PASS}@cluster0.nhkunzd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async () => {
+    try{
+        await client.connect();
+        const database = client.db("randomproduct").collection("product");
+      app.get('/products',(req,res) =>{
+        const query = {};
+        const cursor = database.find(query);
+        res.send(cursor);
+      })
+        // await cursor.forEach(console.dir);
+    }
+    finally{
 
+    }
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
